4e7a1acc0c11842a383e8dd3dc733aae6668a216
ataberkakkan <akkanataberk@gmail.com>
2025-07-31 17:08:16 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
